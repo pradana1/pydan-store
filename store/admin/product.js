81fdb1b371340c005818732856a8bfc3667b4c1b
pradana1 <pradanayp19@gmail.com>
2024-10-26 16:46:08 +0700

    admin-product action

--- store/admin/product.js
+++ store/admin/product.js
@@ -7,6 +7,9 @@ export const state = () => ({
     // page
     page: 1,
 
+    // product: {}
+    product: {}
+
     
 
 })
@@ -26,6 +29,13 @@ export const mutations = {
 
         // set value state "page"
         state.page = payload
+    },
+
+    // mutation "SET_PRODUCT_DATA"
+    SET_PRODUCT_DATA(state, payload) {
+
+        // set value state "product"
+        state.product = payload
     },
 }
 
@@ -83,6 +93,74 @@ export const actions = {
 
         })
     },
+
+    // get detail product
+    getDetailProduct({ commit }, payload) {
+
+        // set promise
+        return new Promise((resolve, reject) => {
+
+            // get to REST API "/api/admin/products/:id" with method "GET"
+            this.$axios.get(`/api/admin/products/${payload}`)
+
+            // success
+            .then(response => {
+
+                // commit to mutation "SET_PRODUCT_DATA"
+                commit('SET_PRODUCT_DATA', response.data.data)
+
+                // resolve promise
+                resolve()
+            })
+        })
+    },
+
+    // update product 
+    updateProduct({ dispatch, commit }, {productId, payload}) {
+
+        // set promise 
+        return new Promise((resolve, reject) => {
+
+            // store to REST API "/api/admin/products/:id" with method "POST"
+            this.$axios.post(`/api/admin/products/${productId}`, payload)
+
+            // success
+            .then(() => {
+
+                // dispatch action "getProductsData"
+                dispatch('getProductsData')
+
+                // resolve promise
+                resolve()
+            })
+
+            // error 
+            .catch(error => {
+                reject(error)
+            })
+        })
+    },
+
+    // destroy products
+    destroyProduct({ dispatch, commit }, payload) {
+
+        // set promise
+        return new Promise((resolve, reject) => {
+
+            // delete to Rest API "/api/admin/products/:id" with method "DELETE"
+            this.$axios.delete(`/api/admin/products/${payload}`)
+
+            // success
+            .then(() => {
+
+                // dispatch action "getProductData"
+                dispatch('getProductsData')
+
+                // resolve promise
+                resolve()
+            })
+        })
+    }
 
 
 }
