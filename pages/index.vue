b83b6cc9ef4068b9f63f612dfecca31c3374b6eb
pradana1 <pradanayp19@gmail.com>
2024-11-03 14:09:49 +0700

    layout-cust

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial/>
+  <div class="mt-custom mb-5">
+    <div class="fade-in">
+      <h1 class="text-center">PYDAN</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+export default {}
 </script>
